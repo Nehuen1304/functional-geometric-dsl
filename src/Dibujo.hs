a278aa33de2e595c705a7b8eb92458e94f55eb23
Nehuen1304 <nehuen.guevara@mi.unc.edu.ar>
2025-03-21 12:48:45 -0300

    Algo de Dibujos

---
+++ src/Dibujo.hs
@@ -0,0 +1,61 @@
+module Dibujo where
+
+-- Definir el lenguaje via constructores de tipo
+data Dibujo a = Basica a | Rotar (Dibujo a)| Espejar (Dibujo a) | Rot45 (Dibujo a)
+    | Apilar Float Float (Dibujo a) (Dibujo a)
+    | Juntar Float Float (Dibujo a) (Dibujo a)
+    | Encimar (Dibujo a) (Dibujo a) 
+    deriving(Eq, Show)
+
+
+-- Composición n-veces de una función con sí misma.
+comp :: (a -> a) -> Int -> a -> a
+comp f 0 a = a 
+comp f n a = comp f (n-1) (f a)
+-- Rotaciones de múltiplos de 90.
+r180 :: Dibujo a -> Dibujo a
+r180 a  = Rotar ( Rotar a ) 
+
+r270 :: Dibujo a -> Dibujo a
+r270 a = Rotar (Rotar (Rotar a)) 
+
+-- Pone una figura sobre la otra, ambas ocupan el mismo espacio.
+(.-.) :: Dibujo a -> Dibujo a -> Dibujo a
+(.-.) = Apilar 1 1
+
+-- Pone una figura al lado de la otra, ambas ocupan el mismo espacio.
+(///) :: Dibujo a -> Dibujo a -> Dibujo a
+(///) = Juntar 1 1 
+
+-- Superpone una figura con otra.
+(^^^) :: Dibujo a -> Dibujo a -> Dibujo a
+(^^^) = Encimar 
+
+-- Dadas cuatro dibujos las ubica en los cuatro cuadrantes.
+cuarteto :: Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
+cuarteto a b c d = (.-.) ((///) a b) ((///) c d)   
+
+-- Una dibujo repetido con las cuatro rotaciones, superpuestas.
+encimar4 :: Dibujo a -> Dibujo a
+encimar4 a = (^^^) ((^^^) ((^^^) a (Rotar a )) (Rotar (Rotar a))) (Rotar (Rotar (Rotar a)))  
+
+-- Cuadrado con la misma figura rotada i * 90, para i ∈ {0, ..., 3}.
+-- No confundir con encimar4!
+ciclar :: Dibujo a -> Dibujo a
+ciclar a = cuarteto a (Rotar a) (Rotar(Rotar a)) (Rotar(Rotar(Rotar a)))
+
+-- Transfomar un valor de tipo a como una Basica.
+pureDib :: a -> Dibujo a
+pureDib   = Basica  
+
+-- map para nuestro lenguaje.
+mapDib :: (a -> b) -> Dibujo a -> Dibujo b
+mapDib = undefined
+
+-- Funcion de fold para Dibujos a
+foldDib :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
+       (Float -> Float -> b -> b -> b) ->
+       (Float -> Float -> b -> b -> b) ->
+       (b -> b -> b) ->
+       Dibujo a -> b 
+foldDib = undefined
---
+++ src/Interp.hs
@@ -0,0 +1,45 @@
+module Interp where
+
+import Dibujo
+import Graphics.Gloss
+import qualified Graphics.Gloss.Data.Point.Arithmetic as V
+import Graphics.Gloss.Data.Vector
+
+-- Gloss provee el tipo Vector y Picture.
+type ImagenFlotante = Vector -> Vector -> Vector -> Picture
+
+type Interpretacion a = a -> ImagenFlotante
+
+mitad :: Vector -> Vector
+mitad = (0.5 V.*)
+
+-- Interpretaciones de los constructores de Dibujo
+
+-- interpreta el operador de rotacion
+interp_rotar :: ImagenFlotante -> ImagenFlotante
+interp_rotar = undefined
+
+-- interpreta el operador de espejar
+interp_espejar :: ImagenFlotante -> ImagenFlotante
+interp_espejar = undefined
+
+-- interpreta el operador de rotacion 45
+interp_rotar45 :: ImagenFlotante -> ImagenFlotante
+interp_rotar45 = undefined
+
+-- interpreta el operador de apilar
+interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+interp_apilar = undefined
+
+-- interpreta el operador de juntar
+interp_juntar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+interp_juntar = undefined
+
+-- interpreta el operador de encimar
+interp_encimar :: ImagenFlotante -> ImagenFlotante -> ImagenFlotante
+interp_encimar = undefined
+
+-- interpreta cualquier expresion del tipo Dibujo a
+-- utilizar foldDib
+interp :: Interpretacion a -> Dibujo a -> ImagenFlotante
+interp = undefined
