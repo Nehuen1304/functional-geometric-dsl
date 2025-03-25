0fba61b51f2a76cf991f88f052b0a558fa96e77a
Andres Villagra <89531860+KaiserOfNone@users.noreply.github.com>
2025-03-25 12:25:15 -0300

    Implemente algunas de las funciones

--- src/Interp.hs
+++ src/Interp.hs
@@ -3,7 +3,7 @@ module Interp where
 import Dibujo
 import Graphics.Gloss
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
-import Graphics.Gloss.Data.Vector()
+import Graphics.Gloss.Data.Vector ()
 
 -- Gloss provee el tipo Vector y Picture.
 type ImagenFlotante = Vector -> Vector -> Vector -> Picture
@@ -13,19 +13,26 @@ type Interpretacion a = a -> ImagenFlotante
 mitad :: Vector -> Vector
 mitad = (0.5 V.*)
 
+invertir :: Vector -> Vector
+invertir = ((-1) V.*)
+
 -- Interpretaciones de los constructores de Dibujo
 
 -- interpreta el operador de rotacion
 interp_rotar :: ImagenFlotante -> ImagenFlotante
-interp_rotar = undefined
+interp_rotar imagen origen ancho alto = imagen (origen V.+ ancho) alto (invertir ancho)
 
 -- interpreta el operador de espejar
 interp_espejar :: ImagenFlotante -> ImagenFlotante
-interp_espejar = undefined
+interp_espejar imagen origen ancho alto = imagen (origen V.+ ancho) (invertir ancho) alto
 
 -- interpreta el operador de rotacion 45
 interp_rotar45 :: ImagenFlotante -> ImagenFlotante
-interp_rotar45 = undefined
+interp_rotar45 imagen origen ancho alto = imagen nOrig nAncho nAlto
+  where
+    nOrig = origen V.+ mitad (ancho V.+ alto)
+    nAncho = mitad (ancho V.+ alto)
+    nAlto = mitad (alto V.- alto)
 
 -- interpreta el operador de apilar
 interp_apilar :: Int -> Int -> ImagenFlotante -> ImagenFlotante -> ImagenFlotante
