fa66f25bd415a7aa8c7879f981ab92503b03d106
Clemente Ivetta Etkin <civetta@mi.unc.edu.ar>
2025-04-04 13:04:51 -0300

    fix(Escher.hs & Main): Escher fix y Main Co-Authored-By: Nehuen <nehuen.guevara@mi.unc.edu.ar>

--- src/Main.hs
+++ src/Main.hs
@@ -4,24 +4,26 @@ import Graphics.Gloss.Interface.IO.Display
 import Graphics.UI.GLUT.Begin
 import Dibujo
 import Interp
-import qualified Basica.Ejemplo as E
 import qualified Graphics.Gloss.Data.Point.Arithmetic as V
-import Basica.Escher
-import Basica.BasicaDoble as B
+import Basica.Escher as B
+import Data.Bool (Bool(True))
+import Graphics.Gloss (display, Display (InWindow))
+import qualified Basica.Comun as B
 
 -- Defino Display de gloss
 win :: Display
-win = InWindow "Doble" (800,800) (100,100)
+win = InWindow "Escher" (800,800) (100,100)
 
 -- Defino Color de fondo
 colorini :: Color
-colorini = aquamarine
+colorini = white
 
 -- Defino la Imagen a mostrar
 picture :: Picture
-picture = interp B.interpBas B.ejemplo (0,0) (800,0) (0,800)
+picture = interp B.interpBas (B.escher 5 B.Triangulo) (0,0) (800,0) (0,800)
 
 -- defino main para mostrar la figura, la defino como display que toma Display, un background color y un Picture
 main :: IO ()
 main = display win colorini picture
+
 
