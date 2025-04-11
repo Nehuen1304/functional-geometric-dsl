0ba64f75ca22253d9942d6ada84cb5d4688d893d
Andres Villagra <89531860+KaiserOfNone@users.noreply.github.com>
2025-04-10 23:27:47 -0300

    Limpiedo de imports

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,12 @@
 module Main where
-import Graphics.Gloss
-import Graphics.Gloss.Interface.IO.Display
-import Graphics.UI.GLUT.Begin
-import Dibujo
-import Interp
-import qualified Graphics.Gloss.Data.Point.Arithmetic as V
+
 import Basica.Escher as B
-import Data.Bool (Bool(True))
-import Graphics.Gloss (display, Display (InWindow))
-import qualified Basica.Comun as B
+import Graphics.Gloss
+import Interp
 
 -- Defino Display de gloss
 win :: Display
-win = InWindow "Escher" (800,800) (100,100)
+win = InWindow "Escher" (800, 800) (100, 100)
 
 -- Defino Color de fondo
 colorini :: Color
@@ -20,10 +14,8 @@ colorini = white
 
 -- Defino la Imagen a mostrar
 picture :: Picture
-picture = interp B.interpBas (B.escher 3 B.FishHDPLUS) (0,0) (800,0) (0,800)
+picture = interp B.interpBas (B.escher 3 B.Triangulo) (0, 0) (800, 0) (0, 800)
 
 -- defino main para mostrar la figura, la defino como display que toma Display, un background color y un Picture
 main :: IO ()
 main = display win colorini picture
-
-
