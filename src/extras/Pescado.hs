fd562e590bee289231b595040b8100273e8843d0
Clemente Ivetta Etkin <civetta@mi.unc.edu.ar>
2025-04-11 12:28:10 -0300

    Agregamos los extras

---
+++ src/extras/Pescado.hs
@@ -0,0 +1,30 @@
+module Main where
+import Graphics.Gloss
+    ( Picture, white, display, Display(InWindow), Color )
+import Graphics.Gloss.Interface.IO.Display
+import Graphics.UI.GLUT.Begin
+import Dibujo
+import Interp
+import qualified Graphics.Gloss.Data.Point.Arithmetic as V
+import Basica.Escher as B
+import Data.Bool (Bool(True))
+import Graphics.Gloss (display, Display (InWindow))
+import qualified Basica.Comun as B
+
+-- Defino Display de gloss
+win :: Display
+win = InWindow "Escher" (800,800) (100,100)
+
+-- Defino Color de fondo
+colorini :: Color
+colorini = white
+
+-- Defino la Imagen a mostrar
+picture :: Picture
+picture = interp B.interpBas (B.escher 3 B.FishHDPLUS) (0,0) (800,0) (0,800)
+
+-- defino main para mostrar la figura, la defino como display que toma Display, un background color y un Picture
+main :: IO ()
+main = display win colorini picture
+
+
